da24ce50f46198eb608c9473094ae45f80ac0951
yamada <sinyamada@het.ph.tsukuba.ac.jp>
2014-05-04 17:59:37 +0900

    add ch.bound.smatrix.sh for check bound

--- ch.bound.smat.sh
+++ ch.bound.smat.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 Conf=700
+LOG=bound.log
+rm $LOG
 
-echo "check number of bound"
+echo "#check binding energy"
+echo "#    binding energy          1/S"
 for bin in {1,10,20,50}
 do
     MAXconf=`expr "$Conf" / "$bin"`
@@ -10,27 +13,15 @@ do
     do
 	for it in {7,8,9}
 	do
-	    echo "bin ="$bin $func "t = "$it    
+	    echo "bin ="$bin $func "t = "$it    |tee -a $LOG
 	    for iconf in `seq 0 1 $MAX`
 	    do
-	#echo $(printf %d $bin)
+		printf "%03d  " $iconf |tee -a $LOG
 		file="spin00.bin${bin}/Smatrix/bin/$(printf %s $func).Smat.$(printf %06d-%06d $MAXconf $iconf).RC16x32_B1830Kud013760Ks013710C1761.it$(printf %02d $it)"
-		num1=$(cat $file |grep -e "-9.9999999999999995e-08")
-		num2=$(cat $file |grep -e "-2.0000000000000000e-03")
-		num3=$(cat $file |grep -e "-1.5000000000000000e+01")		
 	    #echo "$file"
-		if [ "-9.0000000000000000e+01" != $(echo $num3 | awk ' { printf "%s\n", $4}') ] 
-		then
-		    echo "binding energy is more large in  $file  (recalulate mroe large Energy range)"
-		fi
-		if [ "0.0000000000000000e+00" != $(echo $num1 | awk ' { printf "%s\n", $4}') -a "-0.0000000000000000e+00" != $(echo $num1 | awk ' { printf "%s\n", $4}')  ] 
-		then
-		    echo "un bound in  $file"
-		fi
-		if [ "0.0000000000000000e+00" != $(echo $num2 | awk ' { printf "%s\n", $4}') -a "-0.0000000000000000e+00" != $(echo $num2 | awk ' { printf "%s\n", $4}')  ] 
-		then
-		    echo "Warrnig shallow in  $file"
-		fi
+		gawk ' { current = $4; val = $2; if(previous != current && NR !=1){printf("%s %s\n",$1 ,$2); count = count + 1;\
+if(val > 1.0){printf("#####################################  warrning  #######################################\n");}} previous = current;}END{if(count == 0){\
+printf("#####################################  warrning  #######################################\n");}}'  $file |tee -a $LOG
 	    done
 	done
 	echo "---------------------------------------------"
